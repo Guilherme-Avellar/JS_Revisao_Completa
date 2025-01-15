1ce010a056b4bce4b89eb287957f0821935c092a
Guilherme <guilhermeavellar25@gmail.com>
2025-01-15 13:49:57 -0300

    explicação de escopo de const

--- scripts/var_let_const_diferencas.js
+++ scripts/var_let_const_diferencas.js
@@ -8,16 +8,21 @@ function exemplo() {
     }
     console.log(x); // 2
 
-    let y = 1;
+    let y = 2;
     if (true) {
         let y = 2; // variável diferente, escopo de bloco
         console.log(y); // 2
     }
     console.log(y); // 1
 
-    const z = 1;
+    const z = 3;
     // z = 2; // erro: não pode reatribuir uma constante
-    console.log(z); // 1
+    console.log(z); // 3
+    // E não tem limitação de escopo, como o let
+    if (true) {
+        console.log(z);
+    }
+    console.log(z);
 }
 
 exemplo();
