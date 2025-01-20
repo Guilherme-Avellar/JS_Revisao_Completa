93e746b59f347d31441f6ed1a87378e039db812e
Guilherme <guilhermeavellar25@gmail.com>
2025-01-20 12:15:33 -0300

    one more exemple of HOISTING

--- scripts/hoisting.js
+++ scripts/hoisting.js
@@ -4,8 +4,16 @@
 // lidas no inicio do programa, mesmo sendo declaradas linhas abaixo
 
 console.log("printe de uma variavel declarada posteriormente: ", x); // x = undefined
-
 var x = 1;
+
+//exemplo 2:
+if(true) {
+    variavelVar = 600
+}
+var variavelVar;
+console.log(variavel); // 600
+
+// Repare que está sendo declarada depois da atribuição da variavelVar
 
 // Entre as variaveis, essa mecânica funciona apenas com o var
 // e mesmo assim, o funcionamento é apenas para não dar erro,
@@ -14,5 +22,8 @@ var x = 1;
 console.log("printe de uma variavel declarada posteriormente: ", y); // erro
 
 let y = 1;
+
+//exemplo2:
+
 
 // Com funções esta mecânica é mais útil
