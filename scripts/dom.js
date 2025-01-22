49e3dc03c9e69600a2ab06a4b510e1881b4fb0fc
Guilherme <guilhermeavellar25@gmail.com>
2025-01-22 20:57:00 -0300

    DOM exemple

---
+++ scripts/dom.js
@@ -0,0 +1,9 @@
+
+// É a representação de um documento HTML em forma de objeto, que pode ser manipulado por scripts.
+
+// document seria uma espécie de "variavel especial", que já está configurada no javascript
+console.log(document);
+
+// É usado para manipular o DOM. Com comandos como:
+//      document.getElementById("id_do_elemento");
+//      document.getElementsByClassName("classe_do_elemento");
