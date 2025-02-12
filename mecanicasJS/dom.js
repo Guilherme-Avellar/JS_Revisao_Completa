4e696bff43af0f5bf9d3fa6efce913c93b84c563
Guilherme <guilhermeavellar25@gmail.com>
2025-02-12 20:40:06 -0300

    DOM exemple

--- mecanicasJS/dom.js
+++ mecanicasJS/dom.js
@@ -7,3 +7,26 @@ console.log(document);
 // É usado para manipular o DOM. Com comandos como:
 //      document.getElementById("id_do_elemento");
 //      document.getElementsByClassName("classe_do_elemento");
+
+// Tome cuidado com .ClassName pois ele retorna um array de elementos, e não um elemento único.
+// Para retornar apenas um você precisa especificar o índice de onde o elemento se encontra. É
+// preferível usar id quando possível, evita certos erros.
+const div1 = document.getElementsByClassName("container")[0];
+const paragrafo = document.getElementById("texto")
+const main = document.querySelector("main");
+
+// Para adicionar elementos no DOM:
+// Primeiro se cria ou manipula o que se quer adicionar
+// criando um parágrafo:
+let p = document.createElement("p");
+p.innerHTML = "texto adicionado por script";
+console.log(p);
+
+// para adicionar uma classe ao elemento:
+p.classList.add("texto-js");
+// Escreve class="texto-js" dentro da tag. Aí o CSS associado é aplicado.
+
+// Adicionando o elemento dentro da tag <div> (exemplo_DOM.html):
+div1.append(p);
+
+
